cbb810277e4541943bf9474f5bcd68f5b2b5f80c
Miguel Román Párraga <mromparb@g.educaand.es>
2022-06-22 22:51:37 +0200

    a

--- database/migrations/2022_06_22_225059_update_centros_table.php
+++ database/migrations/2022_06_22_225059_update_centros_table.php
@@ -13,14 +13,8 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('centros', function (Blueprint $table) {
-            $table->id();
-            $table->string("nombre", 60);
-            $table->string("nombre_legal", 60);
-            $table->string("nif", 9);
-            $table->string("telefono", 9);
-            $table->string("direccion", 160);
-            $table->timestamps();
+        Schema::table('centros', function (Blueprint $table) {
+            $table->foreignId("tarifaSeleccionada")->nullable()->constrained()->references("id")->on("tarifas");
         });
     }
 
@@ -31,6 +25,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('centros');
+        //
     }
 };
